2b84fb497b7e496d1d9186679d6cd191e303cbcd
m6ydz642 <45617447+m6ydz642@users.noreply.github.com>
2021-03-12 13:32:10 +0900

    data table row 등 추가

--- WPF_Tranning/ModelAndView/GameStartViewModel.cs
+++ WPF_Tranning/ModelAndView/GameStartViewModel.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,75 +12,118 @@ using WPF_Tranning;
 
 namespace WPF_Tranning
 {
+    public interface IBaseCommand : ICommand
 
+    {
+
+        void OnCanExecuteChanged();
+
+    }
     public class GameStartViewModel : INotifyPropertyChanged
     {
-    
-        public ICommand ClickKeyPad // 키패드 클릭
-        {
-            get; set;
-        }
+
+        public ICommand ClickKeyPad { get; set; } // 1~9까지 버튼 키패드 클릭
+        public ICommand BackSpaceCommand { protected set; get; } // 한칸씩 삭제 버튼
+        public ICommand Retry { get; set; } // 재시작 버튼
+
+        public ICommand Enter { get; set; } // 키패드 버튼 입력 후 게임 시작 버튼
+
+        public bool StatusGateStart { set; get; } // 게임시작여부
 
         //View와 바인딩된 속성값이 바뀔때 이를 WPF에게 알리기 위한 이벤트
         public event PropertyChangedEventHandler PropertyChanged;
 
         //출력될 문자들을 담아둘 변수
 
-        string _inputString;
+        public string _inputString;
 
         //계산기화면의 출력 텍스트박스에 대응되는 필드
         string _displayText;
 
         CheckValue _checkvalue;
 
+ 
+
+
+
+        /**********************************************************************/
+        public DataTable _datatable;
+
+        
+        public DataTable DataTable
+        {
+            get { return _datatable; }
+            set
+            {
+                _datatable = value;
+                MessageBox.Show("데이터 테이블");
+             //   RaisePropertyChanged("DataTable");
+            }
+        }
+        /**********************************************************************/
+        public interface IBaseCommand : ICommand
+        {
+               void OnCanExecuteChanged();
+        }
+
         public GameStartViewModel() // 기본생성자
         {
+    
             _inputString = "";
             _displayText = "";
             //    this.ClickKeyPad = new RelayCommand(new Action<object>(this.KeyPadClick)); // 아직은 안쓰는데 조만간 뭐 전달한다고 쓸듯
-            this.ClickKeyPad = new AddNumberKeyPad(this); // 키패드 클릭시 값 전달
-                                                          //숫자 버튼을 클릭할 때 실행
-                                                          // this.ClickKeyPad = new AddNumberKeyPad(this);                                          
+            
+            this.ClickKeyPad = new AddNumberKeyPad(this); // 키패드 클릭시 객체 전달
+                                                          //숫자 버튼을 클릭할 때 실행                                 
+            this.BackSpaceCommand = new BackSpaceCommand(this);
             this._checkvalue = new CheckValue();
-        }
+            this.Retry = new Retry(this);
+            this.Enter = new EnterGame(this);
 
        
+            //   Enter = new RelayCommand(new Action<object>(Enter.Execute));
 
-        /*  public ICommand ClickCommand
+            _datatable = new DataTable();
+            _datatable.Columns.Add("count");
+            _datatable.Columns.Add("사용자 입력");
+            _datatable.Columns.Add("점수");
 
-          {
+      
 
-              get
-
-              {
-
-                  return this._clickCommand ??
-
-                  (this._clickCommand = new RelayCommand(ExecuteClick, CanExecuteClick));
-
-              }
+        }
 
 
-          }
-  */
+        private void RaisePropertyChanged(string propertyName)
+        {
+            // take a copy to prevent thread issues
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
 
-  
+
         public string InputString
         {
-            internal set
+             set
             {
-                if (_inputString != value && _checkvalue.checkKeypadLength(InputString))
+                if (_inputString != value/* && _checkvalue.checkKeypadLength(InputString)*/)
                 {
-                    _inputString = value + " ";
+                    _inputString = value;
+              
                     OnPropertyChanged("InputString"); // 값이 들어왔으면 PropertyChanged를 호출함 (변경되었다고 알림)
-
-                    if (value != "")
-                    {
-                        DisplayText = value; // 공백추가
-                    }
+                    // ((BackSpaceCommand)this.BackSpaceCommand).OnCanExecuteChanged();
+                     ((BackSpaceCommand)BackSpaceCommand).OnCanExecuteChanged();
+        
+                        // gamestartviewmodel을 backspacecommand로 형변환
+                 /*   if (value != "") // 이거때문에 마지막 안지워지는거였네 ㅅㅂ ㅋㅋㅋㅋ
+                    {*/
+                        DisplayText = value; // 디스플레이에 전달
+                   // }
 
                 }
-              
+
 
             }
             get { return _inputString; }
@@ -89,30 +133,20 @@ namespace WPF_Tranning
 
         public string DisplayText
         {
-            internal set
+            set
             {
-                if (_displayText != value)
+                if (_displayText != value) // 이거 용도가 뭐지 ㅡ.ㅡ
                 {
                     _displayText = value;
                     OnPropertyChanged("DisplayText"); // 값이 들어왔으면 PropertyChanged를 호출함 (변경되었다고 알림)
-                    // 디스플레이 표시
+                                                      // 디스플레이 표시
+                    ((Retry)Retry).OnCanExecuteChanged();
                 }
             }
             get { return _displayText; }
 
         }
-  
-      
-       
 
-     
-
-
-        private void KeyPadClick(object obj) // 1~9클릭시, 이 함수 안씀 ㅋㅋ
-
-        {
-           
-        }
 
         protected void OnPropertyChanged(string propertyName)
 
@@ -121,7 +155,7 @@ namespace WPF_Tranning
             if (PropertyChanged != null)
 
             {
-            //    MessageBox.Show("프로퍼티 체인지!!!");
+                //    MessageBox.Show("프로퍼티 체인지!!!");
                 PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
 
             }
@@ -129,31 +163,6 @@ namespace WPF_Tranning
 
 
     }
-
-    /*   private void number_click(object sender, RoutedEventArgs e) { 
-       {
-       // keypad.Content = ""; // 초반에 창 변경할 때 Content내용에 숫자를 입력하세요 가 있어서 초기화 해야 됨 
-       // place holder처럼 할랬는데 호출할때 마다 초기화 되서 안됨 ㅠ
-
-       int inputNumberLength = keypad.Content.ToString().Length + 2;  // 초반엔 입력 초기값이 0부터 시작해서 입력값을 미리 한개 입력해놈 (+2)로, 클릭하기 전이라 2로 초기값을 줌
-                                                                      // 0 -> 4- > 6순으로 올라감 
-               _clickCommand.ky();
-       // 처음에 초기값으로 2받은거랑 두번째에 숫자 클릭할때 값이  2번째에 합쳐셔서 나타나기 때문임
-
-       if (inputNumberLength <= 6)
-       {
-           var btn = sender as Button; // btn 전에 한번 검사
-           if (btn == null) return;
-           keypad.Content = (string)keypad.Content + (string)btn.Content + " "; // 여기서는 또 Content로 하네 ㅡ.ㅡ 걍 text가 편한데
-       }
-       else
-       {
-           MessageBox.Show("더 이상 수를 입력할 수 없습니다");
-       }
-
-   }*/
-
-
 
     public class RelayCommand : ICommand
 
@@ -192,9 +201,9 @@ namespace WPF_Tranning
         public bool CanExecute(object parameter)
 
         {
-           
+
             return this.canExecute == null || this.canExecute(parameter);
-          
+
 
         }
 
@@ -206,6 +215,6 @@ namespace WPF_Tranning
             this.execute(parameter ?? "널이얌"); // 앙 파라메터띠
         }
 
-       
+
     }
 }
