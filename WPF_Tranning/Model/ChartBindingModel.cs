89c2f2ff4922ec5eb47f5c058abeeca4345f7c92
m6ydz642 <m6ydz642@gmail.com>
2021-05-02 01:25:20 +0900

    데이터 테이블 차트 바인딩

--- WPF_Tranning/Model/ChartBindingModel.cs
+++ WPF_Tranning/Model/ChartBindingModel.cs
@@ -11,15 +11,6 @@ namespace WPF_Tranning.Model
     {
         public string Argument { get; set; }
         public double Value { get; set; }
-        public static ObservableCollection<ChartBindingModel> GetDataPoints()
-        {
-            return new ObservableCollection<ChartBindingModel> {
-                    new ChartBindingModel { Argument = "Asia", Value = 5.289D},
-                    new ChartBindingModel { Argument = "Australia", Value = 2.2727D},
-                    new ChartBindingModel { Argument = "Europe", Value = 3.7257D},
-                    new ChartBindingModel { Argument = "North America", Value = 4.1825D},
-                    new ChartBindingModel { Argument = "South America", Value = 2.1172D}
-                   };
-        }
+
     }
 }
--- WPF_Tranning/ModelAndView/ChartModelView.cs
+++ WPF_Tranning/ModelAndView/ChartModelView.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,11 +11,31 @@ namespace WPF_Tranning.ModelAndView
 {
     class ChartModelView
     {
-        public ObservableCollection<ChartBindingModel> Data { get; private set; }
+        // 참고사이트 : https://docs.devexpress.com/WPF/9757/controls-and-libraries/charts-suite/chart-control/getting-started/lesson-1-bind-chart-series-to-data
+        public DataTable Data { get; set; }
 
+        ChartBindingModel model;
+        public DataTable GetDataPoints()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Argument");
+            dt.Columns.Add("Value");
+
+            dt.Rows.Add( model.Argument = "Asia", model.Value = 5.289D);
+            dt.Rows.Add( model.Argument = "Australia", model.Value = 2.2727D);
+            dt.Rows.Add( model.Argument = "Europe", model.Value = 3.7257D);
+            dt.Rows.Add( model.Argument = "North America", model.Value = 3.7257D);
+            dt.Rows.Add( model.Argument = "South America", model.Value = 2.1172D);
+
+
+            return dt;
+        
+        }
         public ChartModelView()
         {
-            this.Data = ChartBindingModel.GetDataPoints();
+            model = new ChartBindingModel();
+            Data = GetDataPoints();
+            
         }
     }
 }
