84c5db33561576e4a423e5b5cffc545170d79fb2
m6ydz642 <m6ydz642@gmail.com>
2021-08-11 23:34:30 +0900

    동일한 시트 삭제 후 추가

--- WPF_Tranning/ModelAndView/SpreadsheetControlMV.cs
+++ WPF_Tranning/ModelAndView/SpreadsheetControlMV.cs
@@ -24,19 +24,28 @@ namespace WPF_Tranning.ModelAndView
         {
             DataModel.CurrentClassPath = typeof(ChartBindingView).FullName; // 현재 접근한 클래스
             IGridSheetLoaded = new RelayCommand(new Action<object>(this.GridSheetControlLoaded));
-            IDeleteSheets = new RelayCommand(new Action<object>(this.deleteSheets));
+            IDeleteSheets = new RelayCommand(new Action<object>(this.deleteSheets_add));
         }
         // private IWorkbook workbook { get; set; } 
         // 전역변수로 해도 되고 이벤트 파라메터에서 다시 형변환해서 써도 되고 
         // MakeDataWorkBooks에서 사용하는 것을 형변환 해서 써도 됨 (어짜피 workbook호출해서 쓰면 SpreadsheetControl에 반영 되어있음)
 
-        private void deleteSheets(object obj)
+        private void deleteSheets_add(object obj)
         {
          //   workbook.Worksheets.Remove(workbook.Worksheets["TestSheet1"]);
          if (obj is SpreadsheetControl control)
             {
                 IWorkbook workbook = control.Document; // 기존 로딩했던 문서를 삭제시 다시 불러와서 작업 할 수 있게 로딩함
                 workbook.Worksheets.Remove(workbook.Worksheets["TestSheet1"]);
+
+                // 삭제후 다른 데이터로 동일 시트 생성 및 추가 
+
+                workbook.Worksheets.Add().Name = "TestSheet1";
+                workbook.Worksheets.ActiveWorksheet.Cells.Style.NumberFormat = "mm/dd/테스트";
+                workbook.Worksheets.ActiveWorksheet.Cells["D11"].Value = "삭제 후 추가";
+                workbook.Worksheets.ActiveWorksheet.Cells["D15"].Value = "삭제 후 workbook값 추가";
+                string getSheetsName = workbook.Worksheets.ActiveWorksheet.ToString(); // 값 확인용
+                Worksheet worksheet = workbook.Worksheets["TestSheet1"]; // sheets이름 얻어와 확인 (workbook으로 해도 되고)
             }
         }
 
