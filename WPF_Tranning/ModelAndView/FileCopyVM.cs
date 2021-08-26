1173434926a0116e9eb5ba119a3ff20296ece5eb
m6ydz642 <m6ydz642@gmail.com>
2021-08-26 22:16:04 +0900

    richeditcontrol 추가

--- WPF_Tranning/ModelAndView/FileCopyVM.cs
+++ WPF_Tranning/ModelAndView/FileCopyVM.cs
@@ -1,22 +1,38 @@
-﻿using System;
+﻿using DevExpress.Xpf.RichEdit;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using WPF_Tranning.Model;
 using WPF_Tranning.View;
 
 namespace WPF_Tranning.ModelAndView
 {
-    class FileCopyVM
-    {
+    class FileCopyVM : INotifyPropertyChanged
 
-       
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ICommand IWord { get; set; }
         public FileCopyVM()
         {
             DataModel.CurrentClassPath = typeof(FileCopyV).FullName; // 현재 접근한 클래스
+
+            IWord = new RelayCommand(new Action<object>(this.RoadRichEditControl));
+
+
         }
+
+        private void RoadRichEditControl(object obj)
+        {
+            var richEditControl1 = (RichEditControl)obj;
+            string htmlText = richEditControl1.HtmlText; // html텍스트 변환
+        }
+
+
     }
 }
--- WPF_Tranning/View/FileCopyV.xaml.cs
+++ WPF_Tranning/View/FileCopyV.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF_Tranning.ModelAndView;
 
 namespace WPF_Tranning.View
 {
@@ -23,8 +24,9 @@ namespace WPF_Tranning.View
         public FileCopyV()
         {
             InitializeComponent();
+            this.DataContext = new FileCopyVM();
         }
 
-     
+   
     }
 }
