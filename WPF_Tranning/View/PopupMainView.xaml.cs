2a2fda5be66434e2aaa1a43cb54d1f57663594ba
m6ydz642 <m6ydz642@gmail.com>
2021-10-09 01:41:44 +0900

    비하인드 코드 Delegate핸들러 완료

--- WPF_Tranning/View/PopupMainView.xaml.cs
+++ WPF_Tranning/View/PopupMainView.xaml.cs
@@ -24,7 +24,7 @@ namespace WPF_Tranning.View
         public delegate void DataPushEventHandler(string main);  // 메인폼 --> 서브폼으로 값 전달 델리게이트
         public delegate void DataGetEventHandler(string sub); // 서브폼 --> 메인폼으로 값 전달 델리게이트
 
-        public DataPushEventHandler DataSetEvent; // 메인에서 서브에게 전달
+       // public DataPushEventHandler DataSetEvent; // 메인에서 서브에게 전달
 
         public PopupMainView()
         {
@@ -36,17 +36,14 @@ namespace WPF_Tranning.View
         {
         }
 
-        private void SetMainPopupData(string main)
-        {
 
-        }
         private void ButtonEdit_DefaultButtonClick(object sender, RoutedEventArgs e)
         {
             Window window = new Window();
             PopupSubView popupSubView = new PopupSubView();
             popupSubView.DataGetEvent += new DataGetEventHandler(this.GetSubPopupData); // 서브에서 메인으로 받음
-            popupSubView.DataSetEvent += new DataPushEventHandler(this.SetMainPopupData); // 메인에서 서브로 전달
-            popupSubView.DataSetEvent("메인에서 전달");
+            // popupSubView.DataSetEvent += new DataPushEventHandler(this.SetMainPopupData); // 메인에서 서브로 전달
+            popupSubView.DataSetEvent("메인에서 서브로 전달"); // 위에꺼 굳이 할 필요없음 (팝업 SubView에서 New EventHandler해서
             window.Content = popupSubView;
             window.Show();
         }
